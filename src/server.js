5b9b8c4f1eafd2d733dddefa0461bd4bf4283cbd
Gustavo <gu.souza1998@yahoo.com.br>
2023-03-01 17:13:01 -0300

    update01.03

--- src/server.js
+++ src/server.js
@@ -1,21 +1,29 @@
-const { response } = require("express");
+require("express-async-errors");
+
+const AppError = require("./utils/AppError");
+
 const express = require("express");
 
+const routes = require("./routes");
+
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
+app.use(routes);
 
-    const {id, user} = request.params;
+app.use(( error, request, response, next) => {
+    if(error instanceof AppError)
+    return response.status(error.statusCode).json({
+        status: "error",
+        message: error.message
+    });
 
-    response.send(`Mensagem ID: ${id}.
-    Para o usuário: ${user}.
-    `);
-});
-
-app.get("/users", (request, response) => {
-    const {page, limit} = request.query;
-
-    response.send(`Página: ${page}. Mostrar: ${limit}`);
+    console.error(error);
+ 
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error",
+    });
 });
 
 const PORT = 3333;
